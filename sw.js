100408ac5f4fdd4076cfedef272db3c254f84f98
Zoli <mz.affabilis@gmail.com>
2026-07-30 17:07:08 +0200

    Automatic SW update v2.1.5 - stable

--- sw.js
+++ sw.js
@@ -32,3 +32,22 @@ self.addEventListener('fetch', (e) => {
     caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+});
